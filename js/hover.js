2614da4c6d9597d5cd192c0c89e1fb820c67ca7f
Timamyll <104847527+Timamyll@users.noreply.github.com>
2022-12-17 04:59:31 +0600

    news

--- js/hover.js
+++ js/hover.js
@@ -4,16 +4,16 @@ new hoverEffect({
   intensity2: 2,
   speedIn: 1,
   //   imagesRatio: 1,
-  image1: "../img/icons/bg-portfolio.png",
-  image2: "../img/icons/bg-portfolio.png",
-  displacementImage: "../img/myDistortionImage.jpg",
+  image1: ".././img/icons/bg-portfolio.png",
+  image2: ".././img/icons/bg-portfolio.png",
+  displacementImage: ".././img/myDistortionImage.jpg",
 });
 new hoverEffect({
   parent: document.querySelector(".my-div1"),
   intensity1: 1,
   intensity2: 2,
   speedIn: 1,
-  image1: "../img/icons/bg-portfolio.png",
-  image2: "../img/icons/bg-portfolio.png",
-  displacementImage: "../img/dot.jpg",
+  image1: ".././img/icons/bg-portfolio.png",
+  image2: ".././img/icons/bg-portfolio.png",
+  displacementImage: ".././img/dot.jpg",
 });
